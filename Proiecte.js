2e2378e6e8358b58e361ae943809763d57316783
AndreiPeligrad <107204648+AndreiPeligrad@users.noreply.github.com>
2022-06-09 19:12:06 +0300

    Add files via upload

---
+++ Proiecte.js
@@ -0,0 +1,35 @@
+ let imaginiGalerie =document.querySelectorAll(".imagini-galerie");
+ let ultimaImagineDeschisa;
+ let latimeFereastra= window.innerWidth;
+
+
+ if(imaginiGalerie){
+    imaginiGalerie.forEach(function(imagine, index){
+        imagine.onclick=function(){
+            let getElementCss=window.getComputedStyle(imagine);
+            let imagineaMare1 = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = imagineaMare1.split("/img/");
+             let imagineaMare = getImgUrlPos[1].replace('")',' ');
+        
+
+
+             getLatestOpenedImg= index + 1 ;
+
+            let container=document.body;
+            let fereastraImagine=document.createElement("div");
+            container.appendChild(fereastraImagine);
+            fereastraImagine.setAttribute("class", "fereastra-img");
+            fereastraImagine.setAttribute("onclick", "inchideImg()");
+
+            let imgNoua=document.createElement("img");
+            fereastraImagine.appendChild(imgNoua);
+            imgNoua.setAttribute("src", "img/" + imagineaMare);
+        }
+    });
+ }
+
+
+
+function inchideImg(){
+    document.querySelector(".fereastra-img").remove();
+}
